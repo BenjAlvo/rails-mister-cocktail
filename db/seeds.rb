7d47474437321ec446f79c20a98e6c39a2332756
Benjamin Alvo <benjamin.alvo@gmail.com>
2019-11-20 19:33:09 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,11 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+lemonade = Cocktail.create(name: "lemonade")
+lemon = Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Dose.create(description: "10ml", cocktail: lemonade, ingredient: lemon)
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
